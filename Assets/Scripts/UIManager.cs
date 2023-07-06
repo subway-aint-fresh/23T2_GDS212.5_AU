28208e22167d56b88169794f9a8d890bf2b37b40
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-07-06 14:18:43 +1000

    Created HP bar
    Hp bar on UI that takes 10 damage every 4 seconds if there are viruses in the scene.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,14 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textOxygen;
     [SerializeField] private TextMeshProUGUI textVirus;
+
+    [SerializeField] private Image healthBar;
+    [SerializeField] private float healthAmount = 100f;
+
+    private float damageInterval = 4f;
+    private float damageTimer = 0f;
+
     void Update()
     {
         textOxygen.text = "Oxygen: " + GameManager.spawnedOxygenCount;
         textVirus.text = "Virus: " + GameManager.virusesCounter;
+
+        if (GameManager.virusesCounter > 0)
+        {
+            damageTimer += Time.deltaTime;
+            if (damageTimer >= damageInterval)
+            {
+                TakeDamage();
+                damageTimer = 0f;
+            }
+        }
+    }
+
+    void TakeDamage()
+    {
+        if (healthAmount > 0)
+        {
+            healthAmount -= 10;
+            healthBar.fillAmount = healthAmount / 100f;
+            Debug.Log(healthAmount);
+        }
+    }
+
+    public void Heal(float healingAmount)
+    {
+        healthAmount += healingAmount;
+        healthAmount = Mathf.Clamp(healthAmount, 0, 100);
+
+        healthBar.fillAmount = healthAmount / 100f;
     }
 }
